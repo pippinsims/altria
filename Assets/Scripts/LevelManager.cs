ec1dd079c09700a4fbd96d3a9634e5f6000a01e7
Biostasis-Enigmatica <zordin.k7@gmail.com>
2026-06-26 00:11:44 -0400

    Feat: Unit spawn system can now spawn player controlled units.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -29,14 +29,23 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         tm = GameObject.Find("Team Manager").GetComponent<TeamManager>();
+        tm = GameObject.Find("Team Manager").GetComponent<TeamManager>();
         var entries = Load(spawnValues);
         for (int i = 0; i < toSpawn.Count; i++)
         {
             var e = entries[i];
             if(toSpawn[i] != null)
             {
-                toSpawn[i].GetComponent<AiUnitController>().team = e.Team;
-                toSpawn[i].GetComponent<AiUnitController>().spawnLocation = new(e.X, e.Y);
+                if(toSpawn[i].GetComponent<AiUnitController>() == null)
+                {
+                    toSpawn[i].GetComponent<PlayerUnitController>().team = e.Team;
+                    toSpawn[i].GetComponent<PlayerUnitController>().spawnLocation = new(e.X, e.Y);
+                }
+                else
+                {
+                    toSpawn[i].GetComponent<AiUnitController>().team = e.Team;
+                    toSpawn[i].GetComponent<AiUnitController>().spawnLocation = new(e.X, e.Y);
+                }
                 Instantiate(toSpawn[i], AIHolder.transform);
             }
         }
